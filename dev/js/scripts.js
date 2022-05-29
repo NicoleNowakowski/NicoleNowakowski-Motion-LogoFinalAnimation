d171628ebdb7577399e2ef1fe1994a843983a4ee
NicoleNowakowski <81652088+NicoleNowakowski@users.noreply.github.com>
2022-05-29 03:43:32 -0400

    finally getting everything into one file

---
+++ dev/js/scripts.js
@@ -0,0 +1,10 @@
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+
+
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin)
+
+const mainTL = gsap.timeline();
+
+mainTL
