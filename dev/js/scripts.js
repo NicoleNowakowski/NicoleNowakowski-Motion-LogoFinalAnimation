6da596d8922a21ebf521de993702fe6ce885a890
NicoleNowakowski <81652088+NicoleNowakowski@users.noreply.github.com>
2022-05-31 02:11:51 -0400

    circle draw in

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,16 +1,20 @@
 import { gsap } from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
-gsap.registerPlugin(GSDevTools, DrawSVGPlugin);
+gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MorphSVGPlugin);
+MorphSVGPlugin.convertToPath("#circle");
+MorphSVGPlugin.convertToPath("#connectinglines");
 
 const mainTL = gsap.timeline();
+
 
 function setStage() {
     let tl = gsap.timeline();
 
     tl.set("#aphrodite",{ alpha:0})
-    .set("circle",{alpha:0})
+    .set("#circle",{ rotate:149, transformOrigin:"center center"})
     .set("#connectinglines",{alpha:0})
     .set("#coffeebean2", {rotate:-300, svgOrigin: "618.36px 356.35px", alpha: 1})
     .set("#coffeebean3", {rotate:-240, svgOrigin: "618.36px 356.35px", alpha: 1})
@@ -28,6 +32,8 @@ function rotateBeans() {
     .to("#coffeebean4", {duration:3, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1},"coffee+=.5")
     .to("#coffeebean5", {duration:3, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1},"coffee+=.5")
     .to("#coffeebean6", {duration:3, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1},"coffee+=.5")
+    .from("#circle", {duration: 2, drawSVG: "0%"})
+   // .to("#circle",{duration: 1, morphSVG:"#connectinglines"});
     
     return tl;
 }
