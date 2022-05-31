6203eb990581e1987fd67f2ee3abbd3274bf91fd
NicoleNowakowski <81652088+NicoleNowakowski@users.noreply.github.com>
2022-05-31 15:10:54 -0400

    aphrodite fades in

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -14,6 +14,7 @@ function setStage() {
     let tl = gsap.timeline();
 
     tl.set("#aphrodite",{ alpha:0})
+    .set("#aphroditecutout",{ alpha:0})
     .set("#circle",{ rotate:149, transformOrigin:"center center"})
     .set("#circle2",{ opacity:0})
     .set("#connectinglines",{alpha:0})
@@ -28,14 +29,16 @@ function setStage() {
 
 function rotateBeans() {
     let tl = gsap.timeline();
-    tl.to("#coffeebean2", {duration:3, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1}, "coffee+=.5")
-    .to("#coffeebean3", {duration:3, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1}, "coffee+=.5")
-    .to("#coffeebean4", {duration:3, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1},"coffee+=.5")
-    .to("#coffeebean5", {duration:3, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1},"coffee+=.5")
-    .to("#coffeebean6", {duration:3, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1},"coffee+=.5")
+    tl.to("#coffeebean2", {duration:2, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1}, "coffee+=.5")
+    .to("#coffeebean3", {duration:2, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1}, "coffee+=.5")
+    .to("#coffeebean4", {duration:2, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1},"coffee+=.5")
+    .to("#coffeebean5", {duration:2, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1},"coffee+=.5")
+    .to("#coffeebean6", {duration:2, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1},"coffee+=.5")
     .from("#circle", {duration: 2, drawSVG: "0%"})
-    .to("#circle2", {opacity:1, morphSVG: {shape: "#connectinglines", shapeIndex: 5, duration: 2}});
-    
+    .to("#circle2", {opacity:1, duration: 1.5, morphSVG: {shape: "#connectinglines", shapeIndex: 5}})
+    .from("#aphroditecutoutstroke", {alpha:1, duration: 2, drawSVG: "0%"},"cutout")
+    .to("#aphroditecutout", {alpha:1, duration:2},"cutout+=.5")
+    .to("#aphrodite", {alpha:1, duration:3})
     return tl;
 }
 
