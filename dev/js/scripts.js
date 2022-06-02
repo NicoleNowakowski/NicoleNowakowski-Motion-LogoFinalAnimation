93e99feccc574d0ab6bc93af53250561b6be9b7d
NicoleNowakowski <81652088+NicoleNowakowski@users.noreply.github.com>
2022-06-01 23:21:48 -0400

    complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -36,15 +36,37 @@ function rotateBeans() {
     .to("#coffeebean4", {duration:1.5, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1},"coffee+=.5")
     .to("#coffeebean5", {duration:1.5, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1},"coffee+=.5")
     .to("#coffeebean6", {duration:1.5, rotate:0, svgOrigin: "618.36px 356.35px", alpha: 1},"coffee+=.5")
-    .from("#circle", {duration: 1.5, drawSVG: "0%"})
+    
+    .to("#beanoutline1", {duration:1.5, stroke:"#DA9C8B"},"color")
+    .to("#beanoutline2", {duration:1.5, stroke:"#DA9C8B"},"color")
+    .to("#beanoutline3", {duration:1.5, stroke:"#DA9C8B"},"color")
+    .to("#beanoutline4", {duration:1.5, stroke:"#DA9C8B"},"color")
+    .to("#beanoutline5", {duration:1.5, stroke:"#DA9C8B"},"color")
+    .to("#beanoutline6", {duration:1.5, stroke:"#DA9C8B"},"color")
+    .to("#innerbean1", {duration:1.5, fill:"#DA9C8B"},"color")
+    .to("#innerbean2", {duration:1.5, fill:"#DA9C8B"},"color")
+    .to("#innerbean3", {duration:1.5, fill:"#DA9C8B"},"color")
+    .to("#innerbean4", {duration:1.5, fill:"#DA9C8B"},"color")
+    .to("#innerbean5", {duration:1.5, fill:"#DA9C8B"},"color")
+    .to("#innerbean6", {duration:1.5, fill:"#DA9C8B"},"color")
+  //note: I tried just giving them all a class so that I wouldn't have to have this much excessive code 
+  //but for some reason it broke and didn't work. :(
+  
+    .from("#circle", {duration: 1.5, drawSVG: "0%"},"color")
     .to("#circle2", {opacity:1, duration: 1, morphSVG: {shape: "#connectinglines", type: "rotational", origin:"center"}})
-    .from("#aphroditecutoutstroke", {alpha:1, duration: 2, drawSVG: "0%"},"cutout")
-    .to("#aphroditecutout", {alpha:1, duration:1.5},"cutout+=1.5")
-    .to("#aphrodite", {alpha:1, duration:2}, "aphrodite")
-    .to("#eye", {alpha:1, duration:2},"aphrodite")
-    .to("#blink", {alpha:1, duration:2},"aphrodite")
-    .to("#eye", {alpha:0, duration:0.2})
-    .to("#eye", {alpha:1, duration:0.2},"aphrodite+=3")
+    .from("#aphroditecutoutstroke", {alpha:1, duration: 1.5, drawSVG: "0%"},"cutout")
+    .to("#aphroditecutout", {alpha:1, duration:1},"cutout+=1.5")
+    .to("#aphrodite", {alpha:1, duration:2.5}, "aphrodite")
+    .to("#eye", {alpha:1, duration:2.5},"aphrodite")
+    .to("#blink", {alpha:1, duration:2.5},"aphrodite")
+
+    .to("#eye", {alpha:0, duration:0.6})
+    .to("#eye", {alpha:1, duration:1},"aphrodite+=2")
+    .to("#eye", {alpha:1, duration:1})
+ 
+    
+   
+    
     return tl;
 }
 
