cb4bcdddd5e21ee5599be77f490a198d2ed0a832
NicoleNowakowski <81652088+NicoleNowakowski@users.noreply.github.com>
2022-06-04 18:26:04 -0400

    fixing blink

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -68,8 +68,8 @@ function rotateBeans() {
     .to("#eye", {alpha:1, duration:2.5},"aphrodite")
     .to("#blink", {alpha:1, duration:2.5},"aphrodite")
 
-    .to("#eye", {alpha:0, duration:0.5},"aphrodite+=2")
-    .to("#eye", {alpha:1, duration:0.8},"aphrodite+=3")
+    .to("#eye", {alpha:0, duration:0.5},"aphrodite+=2.5")
+    .to("#eye", {alpha:1, duration:0.8},"aphrodite+=3.5")
     .to("#eye", {alpha:1, duration:0.5})
  
     
